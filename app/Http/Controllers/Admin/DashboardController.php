c01abf48ac164b59aa312aa151ac38296bd5d974
Ari Padrian <32402214+aripadrian@users.noreply.github.com>
2020-12-27 12:33:25 +0700

    change dashboard controller

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -7,8 +7,9 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
+        // kalo parameter Request $request cuma dipake kalo ada parameter yang di oper kal
         return view('pages.admin.dashboard');
     }
 }
